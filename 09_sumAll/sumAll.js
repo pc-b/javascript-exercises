f15e75aecde74474e8d739d54b4b28ca3ce23a29
margo <aidanmclg@gmail.com>
2025-05-25 22:35:22 -0400

    js exercises 1

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (i, j) {
+    let sum = 0;
 
+    if (typeof i !== "number" ||
+        typeof j !== "number" ||
+        !Number.isInteger(i) ||
+        !Number.isInteger(j) ||
+        i < 0 ||
+        j < 0) {
+        return "ERROR";
+    }
+
+
+
+    for (let from = Math.min(i, j); from <= Math.max(i, j); from++) {
+        sum += from;
+    }
+    return sum;
 };
 
 // Do not edit below this line
